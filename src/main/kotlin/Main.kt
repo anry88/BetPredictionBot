5e90bf2d94f37080bd0c34b02f40c913df4d8a51
Andrey Brazhnikov <anry15@mail.ru>
2024-06-14 21:16:32 +0300

    woring football data api

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,16 +2,14 @@ import dto.MatchResponse
 import org.telegram.telegrambots.meta.TelegramBotsApi
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
 import org.slf4j.LoggerFactory
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import service.footballDataApi
+import service.HttpFootBallDataService
 
 fun main() {
     val logger = LoggerFactory.getLogger("Main")
     val botsApi = TelegramBotsApi(DefaultBotSession::class.java)
 
-    val telegramBotToken: String = Config.getProperty("telegram.bot.token") ?: throw IllegalStateException("Telegram Token not found")
+    val telegramBotToken: String =
+        Config.getProperty("telegram.bot.token") ?: throw IllegalStateException("Telegram Token not found")
 
     try {
         botsApi.registerBot(FootballBot(telegramBotToken))
@@ -19,36 +17,7 @@ fun main() {
     } catch (e: Exception) {
         logger.error("Failed to start football bot", e)
     }
-    fetchMatches()
-//    startDailyChatGPTRequest()
-}
-//fun startDailyChatGPTRequest() {
-//    val dailyRequestJob = CoroutineScope(Dispatchers.IO).launch {
-//        while (isActive) {
-//            val matchList = ChatGPTService.getMatchListFromChatGPTWithRetry()
-//            println(matchList) // Здесь вы можете добавить код для обработки полученных данных
-//
-//            delay(TimeUnit.DAYS.toMillis(1))
-//        }
-//    }
-//}
-fun fetchMatches() {
-    val call = footballDataApi.getMatches()
-    call.enqueue(object : Callback<MatchResponse> {
-        override fun onResponse(call: Call<MatchResponse>, response: Response<MatchResponse>) {
-            if (response.isSuccessful) {
-                val matches = response.body()?.matches
-                // Process the matches data as needed
-                matches?.forEach { match ->
-                    println("Match: ${match.homeTeam.name} vs ${match.awayTeam.name}, Score: ${match.score.fullTime.homeTeam} - ${match.score.fullTime.awayTeam}")
-                }
-            } else {
-                println("Request failed with code: ${response.code()}")
-            }
-        }
 
-        override fun onFailure(call: Call<MatchResponse>, t: Throwable) {
-            println("Request failed with error: ${t.message}")
-        }
-    })
+    val footballDataService = HttpFootBallDataService()
+    footballDataService.fetchMatches()
 }
--- src/main/kotlin/service/HttpFootBallDataService.kt
+++ src/main/kotlin/service/HttpFootBallDataService.kt
@@ -2,11 +2,17 @@ package service
 
 import Config
 import FootBallDataApi
+import dto.MatchResponse
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response as RetrofitResponse
 
 class HttpFootBallDataService : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
@@ -16,17 +22,46 @@ class HttpFootBallDataService : Interceptor {
         val request = requestBuilder.build()
         return chain.proceed(request)
     }
+
+    private val apiKey: String = Config.getProperty("football-data.api.token") ?: throw IllegalStateException("API Key not found")
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(this)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://api.football-data.org/")
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val footballDataApi: FootBallDataApi = retrofit.create(FootBallDataApi::class.java)
+
+    fun fetchMatches() {
+        val currentDate = LocalDate.now()
+        val nextDay = currentDate.plusDays(2)
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+
+        val formattedCurrentDate = currentDate.format(formatter)
+        val formattedNextDay = nextDay.format(formatter)
+
+        val call = footballDataApi.getMatches("SCHEDULED", formattedCurrentDate, formattedNextDay)
+        call.enqueue(object : Callback<MatchResponse> {
+            override fun onResponse(call: Call<MatchResponse>, response: RetrofitResponse<MatchResponse>) {
+                if (response.isSuccessful) {
+                    val matches = response.body()?.matches
+                    // Process the matches data as needed
+                    matches?.forEach { match ->
+                        println("Match: ${match.homeTeam.name} vs ${match.awayTeam.name}, Score: ${match.score.fullTime.homeTeam} - ${match.score.fullTime.awayTeam}")
+                    }
+                } else {
+                    println("Request failed with code: ${response.code()}")
+                }
+            }
+
+            override fun onFailure(call: Call<MatchResponse>, t: Throwable) {
+                println("Request failed with error: ${t.message}")
+            }
+        })
+    }
 }
-private val apiKey: String = Config.getProperty("football-data.api.token") ?: throw IllegalStateException("API Key not found")
-
-private val client = OkHttpClient.Builder()
-    .addInterceptor(HttpFootBallDataService())
-    .build()
-
-private val retrofit = Retrofit.Builder()
-    .baseUrl("https://api.football-data.org/")
-    .client(client)
-    .addConverterFactory(GsonConverterFactory.create())
-    .build()
-
-val footballDataApi: FootBallDataApi = retrofit.create(FootBallDataApi::class.java)
