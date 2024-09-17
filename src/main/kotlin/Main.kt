8e73e883e016bc47253ef808a7f81c25dc79311e
Andrey Brazhnikov <anry15@mail.ru>
2024-09-17 14:05:29 +0200

    Добавлен джоб для обновления матчей и отправки сообщений каждый час

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -98,7 +98,7 @@ fun main() {
 
     val dailyUpdateTrigger = TriggerBuilder.newTrigger()
         .withIdentity("updateMatchesDailyTrigger", "group1")
-        .withSchedule(CronScheduleBuilder.cronSchedule("0 0 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * ?"))
+        .withSchedule(CronScheduleBuilder.cronSchedule("0 5 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * ?"))
         .build()
 
     // Добавляем новый job для отправки сообщений с точностью предсказаний
